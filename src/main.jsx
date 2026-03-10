e3ff800a9bda7ddab2b57183a85bb32de847707a
anantakrroy <kr.innovate@gmail.com>
2026-03-10 13:37:01 +0530

    tailwind setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
